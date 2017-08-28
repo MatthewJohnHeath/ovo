be81212077f0ff8bb089b65c240f5f4adfe9839d
MatthewJohnHeath <matthew.john.heath@gmail.com>
2017-08-28 21:23:36 +0100

    parsing of line.

--- AddressBook.hs
+++ AddressBook.hs
@@ -1,6 +1,6 @@
-import Data.Time
-module AddressBook (Gender, AddressBookEntry, AddressBook) where
 
+module AddressBook (Gender, AddressBookEntry, AddressBook) where
+import Data.Time
 
 data Gender = 
 	Male |Female
@@ -11,8 +11,27 @@ data  AddressBookEntry = AddressBookEntry
 		firstName	:: String,
 		lastName	:: String,
 		gender		:: Gender,
-		date		:: DateTime
+		date		:: Day
 	} deriving(Eq)
 
-data AddressBook = [AddressBookEntry]
+data AddressBook = List AddressBookEntry
 
+parseGender :: String -> Maybe Gender
+parseGender "Male" = Just Male
+parseGender "Female" = Just Female
+parseGender _ = Nothing
+
+parseDay :: String -> Maybe Day
+parseDay  = parseTime defaultTimeLocale "%d%m%Y" 
+
+fillVals :: [String] -> Maybe AddressBookEntry
+fillVals [fn, ln, gen, d] =
+	AddressBookEntry fn ln <$> (parseGender gen) <*> (parseDay d)
+fillVals _ = Nothing	
+
+-- parseLine :: String ->  Maybe AddressBookEntry
+	-- AddressBook <*> values!!0 
+
+
+-- parseAddressBook :: String -> AddressBook 
+-- parseAddressBook file
