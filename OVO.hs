dcf075d90490c913a925c653f655aba30dfd4c4d
MatthewJohnHeath <matthew.john.heath@gmail.com>
2017-08-31 21:22:24 +0100

    comments. line numbers.  Also removed list of questions for iterating through; it made things less clear

--- OVO.hs
+++ OVO.hs
@@ -3,6 +3,6 @@ import Questions
 
 main = do
     addressBook <- readAddressBook "addressBook.txt"
-    putStrLn (countMales addressBook)
-    putStrLn (eldest addressBook)
-    putStrLn (ageDifferenceString addressBook)
+    putStrLn ("1. " ++ countMales addressBook)
+    putStrLn ("2. " ++ eldest addressBook)
+    putStrLn ("3. " ++ ageDifferenceString addressBook)
--- Questions.hs
+++ Questions.hs
@@ -3,28 +3,26 @@ import AddressBook
 import Data.Time
 import Data.List
 import Data.Ord
-
+--Counts males
 countMales :: AddressBook -> String
 countMales = show . length . filter ( (==) Male . gender) 
-
+--returns eldest
 eldest :: AddressBook -> String
 eldest book = 
     firstName entry ++ " " ++ lastName entry
     where
         entry = minimumBy (comparing date) book
     
-
+--Will return Just DIFFERENCE where DIFFERENCE is difference in age between *a* Bill and *a* Paul if there is at least on of each. Else returns Nthing
 ageDifference :: AddressBook -> Maybe Integer
 ageDifference book = 
     diffDays <$>(dateFor "Paul") <*> (dateFor "Bill") 
     where
         dateFor name = date <$> ( find ((==)name . firstName) book )
-
+--Converts ageDifference to string else complains
 ageDifferenceString:: AddressBook -> String
 ageDifferenceString book = case ageDifference book of 
                             Just n ->show n
                             Nothing -> "Look up failed"
 
-questions :: [AddressBook -> String]
-questions = [countMales, eldest, ageDifferenceString]
 
